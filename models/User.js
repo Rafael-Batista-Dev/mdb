7b711e651aa4c4c0f283ea7618d7731a68610f33
Rafael-Batista-Dev <59888617+Rafael-Batista-Dev@users.noreply.github.com>
2020-02-25 20:47:50 -0300

    Implementação no controller e model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,34 @@
 class User {
-  constructor() {
-    this._name = name;
-    this._name = name;
-    this._name = name;
-    this._name = name;
-    this._name = name;
-    this._name = name;
-    this._name = name;
-    this._name = name;
+  constructor(
+    nome,
+    nascimento,
+    celular,
+    email,
+    cep,
+    logradouro,
+    bairro,
+    cidade,
+    estado,
+    numeroEnd,
+    senha,
+    genero,
+    admin,
+    foto
+  ) {
+    this._nome = nome;
+    this._nascimento = nascimento;
+    this._celular = celular;
+    this._email = email;
+    this._cep = cep;
+    this._logradouro = logradouro;
+    this._bairro = bairro;
+    this._cidade = cidade;
+    this._estado = estado;
+    this._numeroEnd = numeroEnd;
+    this._senha = senha;
+    this._genero = genero;
+    this._admin = admin;
+    this._foto = foto;
+    this._registroData = new Date();
   }
 }
